78657a51ebf1b1b9948acb3ad891055935ecea07
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-14 14:16:20 -0800

    set up CRUD router requests in seeds-router

--- data/seeds/seeds-router.js
+++ data/seeds/seeds-router.js
@@ -4,5 +4,36 @@ const db = require('../db');
 const router = express.Router();
 // creates a new sub-Router
 
+// POST
+router.post('/', (request, response) => {
+
+})
+
+router.post('/', (request, response) => {
+
+})
+
+// GET
+router.get('/', (request, response) => {
+
+})
+
+router.get('/', (request, response) => {
+
+})
+
+router.get('/', (request, response) => {
+
+})
+
+// PUT
+router.put('/', (request, response) => {
+
+})
+
+// Delete
+router.delete('/', (request, response) => [
+    
+])
 
 module.exports = router;
